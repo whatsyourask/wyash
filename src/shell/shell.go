7bad7ee7607df8305a148fd9a88375b2a934a709
whatsyourask <lexeymartynov@gmail.com>
2022-07-27 18:21:40 +0300

    Completed shell command execution, but for some reason it doesn't work on one command without arguments

--- src/shell/shell.go
+++ src/shell/shell.go
@@ -8,16 +8,21 @@ import (
 	"strings"
 	"os/exec"
 	"runtime"
+	"reflect"
 )
 
 func ShellLoop() {
 	const cmdPrompt = "wyash$ "
+	const exit = "exit\n"
 	cmdExecStatus := true
 	for cmdExecStatus {
 		fmt.Print(cmdPrompt)
 		cmd := shellReadCmd()
+		if cmd == exit {
+			os.Exit(1)
+		}
 		cmdArgs := shellGetArgs(cmd)
-		cmdExecStatus = shellRun(cmdArgs)
+		shellRun(cmdArgs)
 	}
 }
 
@@ -27,6 +32,8 @@ func shellReadCmd() string {
 	if err != nil {
 		dieWithLog(traceFunc() + ": error of input reading.")
 	}
+	cmdLen := len(cmd)
+	cmd = cmd[0:cmdLen - 1]
 	return cmd
 }
 
@@ -41,46 +48,56 @@ func shellGetArgs(cmd string) []string {
 	return cmdArgs
 }
 
-var builtins = map[string]func([]string)bool{
+var builtins = map[string]func([]string){
 	"cd": shellCd,
 }
 
-func shellRun(cmdArgs []string) bool {
+func shellRun(cmdArgs []string) {
+	fmt.Println(traceFunc(), "cmdArgs = ", cmdArgs)
 	for builtin, builtin_func := range builtins {
 		if builtin == cmdArgs[0] {
-			return builtin_func(cmdArgs)
+			fmt.Println(traceFunc(), "cmdArgs[0] = " + cmdArgs[0])
+			builtin_func(cmdArgs)
+			return
 		}
 	}
-	return shellExecCmd(cmdArgs)
+	shellExecCmd(cmdArgs)
 }
 
-func shellCd(cmdArgs []string) bool {
-	fmt.Println("CD")
+func shellCd(cmdArgs []string) {
+	fmt.Println(traceFunc(), "CD")
 	cmdArgsLen := len(cmdArgs)
 	if cmdArgsLen < 2 {
-		dieWithLog(traceFunc() + ": argument required.")
+		os.Chdir("..")
 	} else {
 		os.Chdir(cmdArgs[1])
 	}
-	return true
 }
 
-func shellExecCmd(cmdArgs []string) bool {
+func shellExecCmd(cmdArgs []string) {
+	fmt.Println(traceFunc(), "cmdArgs = ", cmdArgs)
 	executable := cmdArgs[0]
-	fmt.Println(executable)
-	execArgs := cmdArgs[0:]
-	fmt.Println(execArgs)
-	cmd := exec.Command(executable, execArgs...)
-	err := cmd.Run()
+	// fmt.Println("executable = ", executable)
+	// fmt.Println("executable type of ", reflect.TypeOf(executable))
+	execArgs := cmdArgs[1:]
+	tmpArgs := strings.Join(execArgs, " ")
+	// execArgsLen := len(execArgs)
+	// execArgs = execArgs[0:execArgsLen - 1]
+	fmt.Println(traceFunc(), "execArgs = ", tmpArgs)
+	fmt.Println(traceFunc(), "execArgs type of ", reflect.TypeOf(tmpArgs))
+	cmdOutput, err := exec.Command(executable, tmpArgs).Output()
+	fmt.Println(traceFunc(), string(cmdOutput))
 	if err != nil {
-		dieWithLog(traceFunc() + ": error of command execution.")
+		fmt.Println(err)
 	}
-	return true
 }
 
 func traceFunc() string {
 	pc := make([]uintptr, 1)
 	runtime.Callers(2, pc)
 	f := runtime.FuncForPC(pc[0])
-	return f.Name()
+	funcName := f.Name()
+	funcNameSplited := strings.Split(funcName, "/")
+	shortFuncName := funcNameSplited[len(funcNameSplited) - 1]
+	return shortFuncName
 }
